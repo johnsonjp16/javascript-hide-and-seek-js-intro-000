441097be448b61b394c8516f41e7f2aa84038376
Jeff Johnson <johnsonjp@yahoo.com>
2018-12-31 19:46:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
 function nestedTarget() {
-  return document.querySelector('#nested .target')
+  return document.getElementById("nested").querySelector(".target");
 }
 
 function increaseRankBy(n) {
-  const rankedLists = document.querySelectorAll('.ranked-list')
-
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    let children = rankedLists[i].children
-
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n
-    }
+  var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for(let i=0;i<lis.length;i++) {
+    lis[i].innerHTML=parseInt(lis[i].innerHTML)+n;
   }
 }
 
 function deepestChild() {
-  let node = document.getElementById('grand-node')
-  let nextNode = node.children[0]
-
-  while (nextNode) {
-    node = nextNode
-    nextNode = node.children[0]
+  var lis=document.getElementById("grand-node").querySelectorAll("div");
+  var test;
+  for(let i=0;i<lis.length-1;i++) {
+    test=lis[i].querySelector("div");
   }
+return test;
 }
